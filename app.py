bc8dfdd8e244c81bcc5e989a85dd9d52f814f379
hudiilfeld <hudifeld@gmail.com>
2018-10-29 21:10:20 +0200

    deleted problematic line of db.create_all() in app.py

--- app.py
+++ app.py
@@ -16,11 +16,6 @@ app.config['SQLALCHEMY_TRACK_MODIFICATIONS'] = False
 app.secret_key = 'Shmuel'
 
 api = Api(app)
-
-@app.before_first_request
-def create_tables():
-	db.create_all()
-
    
 jwt = JWT(app, authenticate, identity) #/auth
 app.config['JWT_EXPIRATION_DELTA'] = timedelta(seconds=3600)
